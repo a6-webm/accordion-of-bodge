0cd0eaf1ffa576328e8fc160ae11980198abf6fe
a6.webm <46289515+a6-webm@users.noreply.github.com>
2022-10-24 23:36:42 +0100

    hook proc now returns 1 on blocked key

--- msg_hook/src/lib.rs
+++ msg_hook/src/lib.rs
@@ -1,37 +1,38 @@
 use std::ptr::null_mut;
 
-use winapi::{ctypes::c_int, shared::{minwindef::{WPARAM, LPARAM, LRESULT, HINSTANCE}, windef::HWND}, um::{winuser::{MSG, HC_ACTION}, libloaderapi::{GetModuleHandleExW, GetModuleHandleW}}};
+use winapi::{ctypes::c_int, shared::{minwindef::{WPARAM, LPARAM, LRESULT}, windef::HWND}, um::{winuser::{MSG, HC_ACTION}}};
 
 static mut GLOB_HWND: HWND = null_mut();
 
 #[no_mangle]
 pub unsafe extern "system" fn set_hwnd(hwnd: HWND) {
+    dbg!(GLOB_HWND);
     GLOB_HWND = hwnd;
+    dbg!(GLOB_HWND);
 }
 
 #[no_mangle]
 pub unsafe extern "system" fn get_msg_proc(code: c_int, w_param: WPARAM, l_param: LPARAM) -> LRESULT {
     use winapi::um::winuser::{CallNextHookEx, WM_INPUT, WM_KEYDOWN, WM_SYSKEYDOWN};
-    match code {
-        HC_ACTION => {
-            let msg: &MSG = &*(l_param as *mut MSG);
-            match (msg.hwnd == GLOB_HWND, msg.message) {
-                (false, WM_INPUT) => {
-                    println!("Blocked keypress");
-                    return CallNextHookEx(null_mut(), 1, w_param, l_param);
-                },
-                (false, WM_KEYDOWN) => {
-                    println!("Blocked keypress");
-                    return CallNextHookEx(null_mut(), 1, w_param, l_param);
-                },
-                (false, WM_SYSKEYDOWN) => {
-                    println!("Blocked keypress");
-                    return CallNextHookEx(null_mut(), 1, w_param, l_param);
-                },
-                (false, _) => {return CallNextHookEx(null_mut(), code, w_param, l_param);},
-                (true, _) => {return CallNextHookEx(null_mut(), code, w_param, l_param);},
-            }
+    if code < 0 || code == HC_ACTION {
+        return CallNextHookEx(null_mut(), code, w_param, l_param);
+    }
+    let msg: &MSG = &*(l_param as *mut MSG);
+    println!("msg_hook: processing msg");
+    match (msg.hwnd == GLOB_HWND, msg.message) {
+        (false, WM_INPUT) => {
+            println!("Blocked keypress");
+            return 1;
         },
-        _ => return CallNextHookEx(null_mut(), code, w_param, l_param),
+        (false, WM_KEYDOWN) => {
+            println!("Blocked keypress");
+            return 1;
+        },
+        (false, WM_SYSKEYDOWN) => {
+            println!("Blocked keypress");
+            return 1;
+        },
+        (false, _) => {return CallNextHookEx(null_mut(), code, w_param, l_param);},
+        (true, _) => {return CallNextHookEx(null_mut(), code, w_param, l_param);},
     }
 }
