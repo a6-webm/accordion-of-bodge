5b6d9c8439a6fc1b80481367bc687b913d92a8fb
a6.webm <a6-webm@users.noreply.github.com>
2022-11-27 18:21:11 +0000

    way

--- accordion_linux/src/main.rs
+++ accordion_linux/src/main.rs
@@ -1,3 +1,9 @@
+use wayland_client::Display;
+
 fn main() {
-    println!("Hello, world!");
+    let display: Display = Display::connect_to_env()
+        .expect("Could not connect to wayland environment");
+    loop {
+        
+    }
 }
