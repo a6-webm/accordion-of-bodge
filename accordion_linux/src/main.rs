fe09d72ac742b4541aa49efe9d0b5200bd0b8bd6
a6.webm <46289515+a6-webm@users.noreply.github.com>
2022-12-24 23:13:10 +0000

    switched to evdev

--- accordion_linux/src/main.rs
+++ accordion_linux/src/main.rs
@@ -1,9 +1,3 @@
-use wayland_client::Display;
-
 fn main() {
-    let display: Display = Display::connect_to_env()
-        .expect("Could not connect to wayland environment");
-    loop {
-        
-    }
+    loop {}
 }
